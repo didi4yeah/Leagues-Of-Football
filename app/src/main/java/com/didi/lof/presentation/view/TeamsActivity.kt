394cbad4164e90d559e1a9e0742f59034184924a
DIAI David <didi.yeah@gmail.com>
2020-10-22 17:54:21 +0200

    Add Main Activity

---
+++ app/src/main/java/com/didi/lof/presentation/view/TeamsActivity.kt
@@ -0,0 +1,12 @@
+package com.didi.lof.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.didi.lof.R
+
+class TeamsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teams)
+    }
+}
